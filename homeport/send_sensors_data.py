5af39236cdcb66d726a61b3f94e4f6cec3f7506e
gmrv <stand6y@gmail.com>
2014-12-08 03:43:55 +0300

    sending sensor data log

--- homeport/send_sensors_data.py
+++ homeport/send_sensors_data.py
@@ -1,5 +1,6 @@
 import socket
 import mygpio
+from datetime import datetime
 
 DEVICE_MAC = 'e8:4e:06:1c:37:e8'
 SENSOR_ID_1 = '28-000005ffad2a'
@@ -11,6 +12,8 @@ try:
     sock.send("#{}\n#{}#{}\n##".format(DEVICE_MAC, SENSOR_ID_1, sensor_value_1))
     data = sock.recv(1024)
     sock.close()
-    print data
+    f = open("sensor_data_send.log", 'a')
+    f.write(str(datetime.now()) + " " + data)
+    f.close()
 except socket.error, e:
     print('ERROR! Exception {}'.format(e))
