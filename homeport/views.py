a81fd206bd1b6a86b254b847327d32de608bac59
gmrv <stand6y@gmail.com>
2014-12-13 05:22:09 +0300

    ip logging

--- homeport/views.py
+++ homeport/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import mygpio
 import os
-import time
+from datetime import datetime
 
 
 html1 = "<html><body><H1><a href='on'>on</a></br></br><a href='off'>off</a></H1></br>"
@@ -13,16 +13,29 @@ port3 = 0
 def home(request):
     html2 = "<b>" + str(mygpio.getTemp()) + "</b>"
 
+    f = open("ip.log", 'a')
+    f.write(str(datetime.now()) + " " + str(request.META['REMOTE_ADDR']) + "\\" + "\r\n")
+    f.close()
 
     return HttpResponse(html1+html2+html3)
 
 def on(request):
     mygpio.setLed(1)
     html2 = "<b>" + str(mygpio.getTemp()) + "</b>"
+
+    f = open("ip.log", 'a')
+    f.write(str(datetime.now()) + " " + str(request.META['REMOTE_ADDR']) + "\\on" + "\r\n")
+    f.close()
+
     return HttpResponse(html1+html2+html3)
 
 def off(request):
     mygpio.setLed(0)
     html2 = "<b>" + str(mygpio.getTemp()) + "</b>"
+
+    f = open("ip.log", 'a')
+    f.write(str(datetime.now()) + " " + str(request.META['REMOTE_ADDR']) + "\\off" + "\r\n")
+    f.close()
+
     return HttpResponse(html1+html2+html3)
 
